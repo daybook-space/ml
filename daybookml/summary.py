1cb0c65c5b79df16c85a896999b52f706d92b2d7
Edward Li <edwardyaoli@gmail.com>
2019-10-26 18:25:22 -0400

    summarize top effectors

---
+++ daybookml/summary.py
@@ -0,0 +1,39 @@
+from .analysis import sm_fun, collapse_items
+
+def top_emotion_effectors(journal_sentiments):
+    events = []
+    people = []
+    locations = []
+    other = []
+
+    for journal in journal_sentiments:
+        events.extend(journal['events'])
+        people.extend(journal['people'])
+        locations.extend(journal['locations'])
+        other.extend(journal['other'])
+
+    events = collapse_items(events)
+    people = collapse_items(people)
+    locations = collapse_items(locations)
+    other = collapse_items(other)
+
+    events.sort(key=lambda x: -sm_fun(x[1], x[2]))
+    people.sort(key=lambda x: -sm_fun(x[1], x[2]))
+    locations.sort(key=lambda x: -sm_fun(x[1], x[2]))
+    other.sort(key=lambda x: -sm_fun(x[1], x[2]))
+
+    event_sm = [(i[0], sm_fun(i[1], i[2])) for i in events]
+    people_sm = [(i[0], sm_fun(i[1], i[2])) for i in people]
+    locations_sm = [(i[0], sm_fun(i[1], i[2])) for i in locations]
+    other_sm = [(i[0], sm_fun(i[1], i[2])) for i in other]
+
+    return {
+        'events': event_sm,
+        'people': people_sm,
+        'locations': locations_sm,
+        'other': other_sm
+    }
+
+def is_suicidal(journal_sentiments):
+    # Assumption is true most of the time
+    return False
